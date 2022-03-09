f99fbac6fbb951babe1889f96a458c2f09f004e0
dante <dante@chxian.com>
2022-03-09 09:57:23 +0800

    feat: 🎸 微调一下全局主题颜色

--- src/components/DTest.vue
+++ src/components/DTest.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="d-test">
-    <div>{{ mainStore.count }}</div>
+    <div class="count_box">{{ mainStore.count }}</div>
 
     <div>{{ mainStore.countMul }}</div>
 
@@ -9,12 +9,14 @@
     <button @click="asyncChange">请求</button>
 
     <n-button type="primary">测试ui按钮</n-button>
+
+    <n-gradient-text type="success" @click="bodyColorChange"> 成了 </n-gradient-text>
   </div>
 </template>
 
 <script setup lang="ts">
 import { useMainStore } from '@/store/main'
-import { NButton } from 'naive-ui'
+import { NButton, NGradientText } from 'naive-ui'
 
 const mainStore = useMainStore()
 
@@ -24,9 +26,35 @@ const change = () => {
   })
 }
 
+const randomArr = Array(10)
+  .fill(0)
+  .map((v, i) => v + i)
+  .concat(['a', 'b', 'c', 'd', 'f'])
+
+const getRgbVal = () => {
+  const i = Math.floor(randomArr.length * Math.random())
+  return randomArr[i]
+}
+
+const getColor = () => {
+  const val = `#${Array(6).fill('').map(getRgbVal).join('')}`
+  console.log(val)
+  return val
+}
+
+const bodyColorChange = () => {
+  const body = document.querySelector<HTMLElement>('body')!
+  // Reflect.set(, '--body-color', getColor())
+  body.style.cssText = `--body-color:  ${getColor()}`
+}
+
 const asyncChange = () => {
   mainStore.countChange()
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.count_box {
+  background-color: $test-color;
+}
+</style>

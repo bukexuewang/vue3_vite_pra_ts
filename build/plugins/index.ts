e1ede1f1c1fa780ab0a8587050374039c8f15120
zhouqian <zhouqian@bailun.com>
2023-05-19 18:48:24 +0800

    perf: ⚡️ 修改了一些东西

--- build/plugins/index.ts
+++ build/plugins/index.ts
@@ -3,6 +3,6 @@ import { AutoPlugins } from './auto';
 import htmlPlugin from './html';
 import compressPlugin from './compression';
 
-export const vitePlugins = (viteEnv: ImportMetaEnv, mode: ) => {
+export const vitePlugins = (viteEnv: ImportMetaEnv, mode: string) => {
   return [htmlPlugin(viteEnv), compressPlugin(viteEnv), ...AutoPlugins, unocssPlugin];
 };
